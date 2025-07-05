da94be3ff0263274af427041c33ff3386b7cfa58
SiaJiaLe <22062566@imail.sunway.edu.my>
2025-07-05 12:55:08 +0800

    TestingCommit

--- main.py
+++ main.py
@@ -63,4 +63,3 @@ def resizeAndOverlayVideo(foregroundVideoPath, backgroundVideoPath):
     backgroundVideo.release()
     cv2.destroyAllWindows()
     
-resizeAndOverlayVideo(r"C:\Users\Sia Jia Le\OneDrive - Sunway Education Group\Digital Image Processing\Group Assignment\CSC2014- Group Assignment_Aug-2025\talking.mp4", r"C:\Users\Sia Jia Le\OneDrive - Sunway Education Group\Digital Image Processing\Group Assignment\CSC2014- Group Assignment_Aug-2025\Recorded Videos (4)\singapore.mp4")
